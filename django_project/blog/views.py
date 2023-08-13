b88867051d1e5abaea0f64ac7018db0e0ff92b7f
Rajat Patil <57714patilrk@gmail.com@gmail.com>
2023-08-13 23:32:02 +0530

    databases and migrations; create users, add and update posts -> using the backend

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'RajatKP',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': 'August 13, 2023',
-    },
-    {
-        'author': 'user2',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': 'August 12, 2023',
-    },
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'title': 'Home',
-        'posts': posts,
+        'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
 
